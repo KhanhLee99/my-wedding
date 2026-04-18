86875aec0fdfd250be6899b6aafe9c7071fae9e1
khanhlv <khanhlv@kaopiz.com>
2026-04-19 00:22:43 +0700

    Refactor music toggle and viewport scripts; remove unused music toggle code and improve viewport handling in JavaScript files. Update HTML structure for better readability. Delete unused wedding images.

--- assets/js/protect_copy.js
+++ assets/js/protect_copy.js
@@ -1,28 +1,31 @@
 window.oncontextmenu = function () {
-                                    return false;
-                                };
-                                document.onkeydown = function (e) {
-                                    if ((e.ctrlKey || e.metaKey) && e.which == 85) {
-                                        e.preventDefault();
-                                        return false;
-                                    }
-                                };
-                                // document.addEventListener("keydown", function (event) {
-                                //     if (
-                                //     event.key === "F12" ||
-                                //     (event.ctrlKey && event.shiftKey && event.key === "I") ||
-                                //     (event.ctrlKey && event.shiftKey && event.key === "J") ||
-                                //     (event.ctrlKey && event.key === "U")
-                                //     ) {
-                                //     event.preventDefault();
-                                //     alert("Không được phép!");
-                                //     }
-                                // });
+  return false;
+};
+document.onkeydown = function (e) {
+  if ((e.ctrlKey || e.metaKey) && e.which == 85) {
+    e.preventDefault();
+    return false;
+  }
+};
+// document.addEventListener("keydown", function (event) {
+//     if (
+//     event.key === "F12" ||
+//     (event.ctrlKey && event.shiftKey && event.key === "I") ||
+//     (event.ctrlKey && event.shiftKey && event.key === "J") ||
+//     (event.ctrlKey && event.key === "U")
+//     ) {
+//     event.preventDefault();
+//     alert("Không được phép!");
+//     }
+// });
 
-                                (function() {
-                                    const styleTitle = 'font-size: 48px; font-weight: bold; color: red;';
-                                    const styleMsg = 'font-size: 16px;';
+(function () {
+  const styleTitle = "font-size: 48px; font-weight: bold; color: red;";
+  const styleMsg = "font-size: 16px;";
 
-                                    console.log('%cDừng lại!', styleTitle);
-                                    console.log('%cĐây là một tính năng của trình duyệt dành cho các nhà phát triển. Nếu ai đó bảo bạn sao chép-dán nội dung nào đó vào đây để bật một tính năng của LadiPage hoặc "hack" tài khoản của người khác, thì đó là hành vi lừa đảo và sẽ khiến họ có thể truy cập vào tài khoản của bạn.', styleMsg);
-                                })();
+  console.log("%cDừng lại!", styleTitle);
+  console.log(
+    '%cĐây là một tính năng của trình duyệt dành cho các nhà phát triển. Nếu ai đó bảo bạn sao chép-dán nội dung nào đó vào đây để bật một tính năng của LadiPage hoặc "hack" tài khoản của người khác, thì đó là hành vi lừa đảo và sẽ khiến họ có thể truy cập vào tài khoản của bạn.',
+    styleMsg
+  );
+})();
--- nha-gai/assets/js/ladipage_run.js
+++ nha-gai/assets/js/ladipage_run.js
@@ -1,41 +1,38 @@
 (function () {
-        var run = function () {
-          if (
-            typeof window.LadiPageScript == "undefined" ||
-            typeof window.ladi == "undefined" ||
-            window.ladi == undefined
-          ) {
-            setTimeout(run, 100);
-            return;
-          }
-          window.LadiPageApp = window.LadiPageApp || new window.LadiPageAppV2();
-          window.LadiPageScript.runtime.ladipage_id =
-            "68ee2b9671dacf0011094d75";
-          window.LadiPageScript.runtime.publish_platform = "WORDPRESS";
-          window.LadiPageScript.runtime.is_mobile_only = true;
-          window.LadiPageScript.runtime.version = "1769744263164";
-          window.LadiPageScript.runtime.cdn_url =
-            "https://w.ladicdn.com/v5/source/";
-          window.LadiPageScript.runtime.DOMAIN_SET_COOKIE = ["jmiiwedding.com"];
-          window.LadiPageScript.runtime.DOMAIN_FREE = [
-            "preview.ldpdemo.com",
-            "ldp.page",
-          ];
-          window.LadiPageScript.runtime.bodyFontSize = 12;
-          window.LadiPageScript.runtime.store_id = "6322a62f2dad980013bb5005";
-          window.LadiPageScript.runtime.store_ladiuid =
-            "6322a612c96988001223c73b";
-          window.LadiPageScript.runtime.time_zone = 7;
-          window.LadiPageScript.runtime.currency = "VND";
-          window.LadiPageScript.runtime.convert_replace_str = true;
-          window.LadiPageScript.runtime.desktop_width = 960;
-          window.LadiPageScript.runtime.mobile_width = 420;
-          window.LadiPageScript.runtime.formdata = true;
-          window.LadiPageScript.runtime.tracking_button_click = true;
-          window.LadiPageScript.runtime.publish_time = 1769757975696;
-          window.LadiPageScript.runtime.lang = "vi";
-          window.LadiPageScript.run(true);
-          window.LadiPageScript.runEventScroll();
-        };
-        run();
-      })();
+  var run = function () {
+    if (
+      typeof window.LadiPageScript == "undefined" ||
+      typeof window.ladi == "undefined" ||
+      window.ladi == undefined
+    ) {
+      setTimeout(run, 100);
+      return;
+    }
+    window.LadiPageApp = window.LadiPageApp || new window.LadiPageAppV2();
+    window.LadiPageScript.runtime.ladipage_id = "68ee2b9671dacf0011094d75";
+    window.LadiPageScript.runtime.publish_platform = "WORDPRESS";
+    window.LadiPageScript.runtime.is_mobile_only = true;
+    window.LadiPageScript.runtime.version = "1769744263164";
+    window.LadiPageScript.runtime.cdn_url = "https://w.ladicdn.com/v5/source/";
+    window.LadiPageScript.runtime.DOMAIN_SET_COOKIE = ["jmiiwedding.com"];
+    window.LadiPageScript.runtime.DOMAIN_FREE = [
+      "preview.ldpdemo.com",
+      "ldp.page",
+    ];
+    window.LadiPageScript.runtime.bodyFontSize = 12;
+    window.LadiPageScript.runtime.store_id = "6322a62f2dad980013bb5005";
+    window.LadiPageScript.runtime.store_ladiuid = "6322a612c96988001223c73b";
+    window.LadiPageScript.runtime.time_zone = 7;
+    window.LadiPageScript.runtime.currency = "VND";
+    window.LadiPageScript.runtime.convert_replace_str = true;
+    window.LadiPageScript.runtime.desktop_width = 960;
+    window.LadiPageScript.runtime.mobile_width = 420;
+    window.LadiPageScript.runtime.formdata = true;
+    window.LadiPageScript.runtime.tracking_button_click = true;
+    window.LadiPageScript.runtime.publish_time = 1769757975696;
+    window.LadiPageScript.runtime.lang = "vi";
+    window.LadiPageScript.run(true);
+    window.LadiPageScript.runEventScroll();
+  };
+  run();
+})();
